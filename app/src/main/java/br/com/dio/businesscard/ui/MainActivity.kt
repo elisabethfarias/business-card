af9317882571355ab9af1d4acb6eafcd8beb7c3a
Elisabeth Farias <elisabethf@ciandt.com>
2021-08-28 16:22:05 -0300

    ViewBinding configuration

---
+++ app/src/main/java/br/com/dio/businesscard/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package br.com.dio.businesscard.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import br.com.dio.businesscard.R
+import br.com.dio.businesscard.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
