5d709763f15ab186ab65dbae6177f1c7fc1c406c
Elisabeth Farias <elisabethf@ciandt.com>
2021-08-29 14:07:04 -0300

    Created BusinessCardAdapter

--- app/src/main/java/br/com/dio/businesscard/ui/MainActivity.kt
+++ app/src/main/java/br/com/dio/businesscard/ui/MainActivity.kt
@@ -3,14 +3,24 @@ package br.com.dio.businesscard.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import br.com.dio.businesscard.App
 import br.com.dio.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+
+    private val adapter by lazy { BusinessCardAdapter() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.rvCards.adapter = adapter
+        getAllBusinessCard()
         insertListener()
     }
 
@@ -20,6 +30,13 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    private fun getAllBusinessCard() {
+        mainViewModel.getAll().observe(this, { businessCards ->
+            adapter.submitList(businessCards)
+        })
+    }
+
 
 
 }
